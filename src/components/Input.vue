b3cc71f4aa90344b730c718efaa4bf9ad24da1da
Volkov Slava <volkov.slava@hotmail.com>
2020-03-03 18:28:48 +0300

    checkbox

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -7,6 +7,20 @@
 
     <textarea v-model="textarea"></textarea>
     <p>{{ textarea }}</p>
+
+    <label>
+      <input type="checkbox" value="instagram" v-model="socials"> Instagram
+    </label>
+    <label>
+      <input type="checkbox" value="vk" v-model="socials"> VK
+    </label>
+    <label>
+      <input type="checkbox" value="facebok" v-model="socials"> Facebook
+    </label>
+    <hr>
+    <ul>
+      <li v-for="social in socials" :key=social.id>{{ social }}</li>
+    </ul>
   </div>
 </template>
 
@@ -15,7 +29,8 @@
     data() {
       return {
         name: 'Initial state',
-        textarea: 'I am initial text'
+        textarea: 'I am initial text',
+        socials: ['vk']
       }
     }
   }

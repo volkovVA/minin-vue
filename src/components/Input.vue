9eed4741a7be87e86d9679cfd4cbf8038e3b8f41
Volkov Slava <volkov.slava@hotmail.com>
2020-03-03 18:20:25 +0300

    textarea

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <h2>Form inputs</h2>
-    
-    <input type="text" v-model.lazy="name">
 
+    <input type="text" v-model.lazy="name">
     <p>{{ name }}</p>
+
+    <textarea v-model="textarea"></textarea>
+    <p>{{ textarea }}</p>
   </div>
 </template>
 
@@ -12,12 +14,20 @@
   export default {
     data() {
       return {
-        name: 'Initial state'
+        name: 'Initial state',
+        textarea: 'I am initial text'
       }
     }
   }
 </script>
 
 <style scoped>
- 
+  textarea {
+    height: 100px;
+    width: 400px;
+  }
+
+  p {
+    white-space: pre;
+  }
 </style>

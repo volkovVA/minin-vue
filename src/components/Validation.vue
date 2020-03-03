d852a308a3eea582cde53d80055b1b0198814f60
Volkov Slava <volkov.slava@hotmail.com>
2020-03-03 21:36:44 +0300

    the validation of the password

--- src/components/Validation.vue
+++ src/components/Validation.vue
@@ -15,24 +15,66 @@
         <div class="invalid-feedback" v-if="!$v.email.required">Email field is required</div>
         <div class="invalid-feedback" v-if="!$v.email.email">This field should be an email</div>
       </div>
+
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password" 
+          class="form-control"
+          :class="{'is-invalid' : $v.password.$error}"
+          @blur="$v.password.$touch()"
+          v-model="password"
+        >
+        <div class="invalid-feedback" v-if="!$v.password.minLength">
+          Min length of password is {{ $v.password.$params.minLength.min }}. Now it is {{ password.length }}.
+        </div>
+      </div>
+
+       <div class="form-group">
+        <label for="confirm">Confirm password</label>
+        <input
+          type="password"
+          id="confirm" 
+          class="form-control"
+          :class="{'is-invalid' : $v.confirmPassword.$error}"
+          @blur="$v.confirmPassword.$touch()"
+          v-model="confirmPassword"
+        >
+        <div class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">
+          Password should match
+        </div>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
 
-  import { required, email } from 'vuelidate/lib/validators'
+  import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
 
   export default {
     data() {
       return {
-        email: ''   
+        email: '',
+        password: '' ,
+        confirmPassword: ''  
       }
     },
     validations: {
       email: {
         required,
         email
+      },
+      password: {
+        minLength : minLength(6)
+      },
+      // confirmPassword: {
+      //   sameAs: sameAs((vue) => {
+      //     return vue.password
+      //   })
+      confirmPassword: {
+        sameAs: sameAs('password')
       }
     }
   }

3fd03ef12f480852ebe5173cff6cf9499462bad5
Volkov Slava <volkov.slava@hotmail.com>
2020-03-04 21:56:48 +0300

    Active link designation

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,18 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav">
-          <li class="nav-item">
+          <!-- <li class="nav-item">
             <router-link class="nav-link" to="/">Home</router-link>
-          </li>
-          <li class="nav-item">
+          </li> -->
+          <!-- <li class="nav-item active" >
             <router-link class="nav-link" :to="'/cars'">Cars</router-link>
-          </li>
+          </li> -->
+          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+            <a class="nav-link">Home</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" to="/cars" active-class="active">
+            <a class="nav-link">Cars</a>
+          </router-link>
         </ul>
       </div>
     </nav>

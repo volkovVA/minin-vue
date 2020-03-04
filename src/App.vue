c45b211119537e08872373a203d0943987dcd796
Volkov Slava <volkov.slava@hotmail.com>
2020-03-04 21:48:59 +0300

    the creation of navigation

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="'/cars'">Cars</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
 
     <app-car>

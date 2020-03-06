ec994846b88cb111e5d3786e1e038a305c076a76
Volkov Slava <volkov.slava@hotmail.com>
2020-03-06 17:52:51 +0300

    getting get objects

--- src/App.vue
+++ src/App.vue
@@ -87,6 +87,17 @@
         <input type="text" id="year" class="form-control" v-model.number="carYear">
       </div>
       <button class="btn btn-success" @click="createCar">Create car</button>
+      <button class="btn btn-primary" @click="loadCars">Load cars</button>
+      <hr>
+      <ul class="list-group">
+        <li 
+          class="list-group-item"
+          v-for="car of cars"
+          :key = "car.id"
+        >
+          <strong>{{ car.name }}</strong> -  {{ car.year }}
+        </li>
+      </ul>
     </div>
     
   </div>
@@ -115,7 +126,8 @@ export default {
       searchName: '',
       switched: false,
       carNameForm: '',
-      carYear: 2018
+      carYear: 2018,
+      cars: []
     }
   },
   methods: {
@@ -131,6 +143,15 @@ export default {
        .then(newCar => {
          console.log(newCar)
        })
+    },
+    loadCars() {
+      this.$http.get('http://localhost:3000/cars')
+        .then(response => {
+            return response.json()
+        })
+        .then(cars => {
+          this.cars = cars
+        })
     }
   },
   directives: {

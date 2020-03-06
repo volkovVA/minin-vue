382d48c05e07f93c995426211f43372ccc7737ff
Volkov Slava <volkov.slava@hotmail.com>
2020-03-06 18:03:46 +0300

    using resource

--- src/App.vue
+++ src/App.vue
@@ -127,7 +127,8 @@ export default {
       switched: false,
       carNameForm: '',
       carYear: 2018,
-      cars: []
+      cars: [],
+      resource: null
     }
   },
   methods: {
@@ -136,23 +137,29 @@ export default {
         name: this.carNameForm,
         year: this.carYear
       }
-      this.$http.post('http://localhost:3000/cars', car )
-        .then(response => {
-          return response.json()
-       })
-       .then(newCar => {
-         console.log(newCar)
-       })
+      // this.$http.post('http://localhost:3000/cars', car )
+      //   .then(response => {
+      //     return response.json()
+      //  })
+      //  .then(newCar => {
+      //    console.log(newCar)
+      //  })
+      this.resource.save({}, car)
     },
     loadCars() {
-      this.$http.get('http://localhost:3000/cars')
-        .then(response => {
-            return response.json()
-        })
-        .then(cars => {
-          this.cars = cars
-        })
+      // this.$http.get('http://localhost:3000/cars')
+      //   .then(response => {
+      //       return response.json()
+      //   })
+      //   .then(cars => {
+      //     this.cars = cars
+      //   })
+      this.resource.get().then(response => response.json())
+        .then(cars => this.cars = cars)
     }
+  },
+  created() {
+    this.resource = this.$resource('http://localhost:3000/cars')
   },
   directives: {
     font: {

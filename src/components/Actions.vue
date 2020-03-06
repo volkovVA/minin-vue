661fa0b3180387d92f72a62575fa67829d6fef9c
Volkov Slava <volkov.slava@hotmail.com>
2020-03-06 19:27:48 +0300

    using mutations

--- src/components/Actions.vue
+++ src/components/Actions.vue
@@ -10,7 +10,8 @@ export default {
   methods: {
     updateCounter(val) {
       // this.$emit('counterUpdated', val)
-      this.$store.state.counter += val
+      // this.$store.state.counter += val
+      this.$store.commit('changeCounter', val)
     }
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,11 @@ export default new Vuex.Store({
   state: {
     counter: 0
   },
+  mutations: {
+    changeCounter(state, payload) {
+      state.counter += payload
+    }
+  },
   getters: {
     computedCounter(state) {
       return state.counter * 10

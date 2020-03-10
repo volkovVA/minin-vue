de80fba1f49cab44c68e2de0662162d36f65b7f6
Volkov Slava <volkov.slava@hotmail.com>
2020-03-10 22:14:22 +0300

    creating a form page

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -1,8 +1,56 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>NewAd</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Create new ad</h1>
+        <v-form ref="form" v-model="valid" validation class="mb-10">
+              <v-text-field
+                label="Ad title"
+                name="title"
+                type="text"
+                required
+                :rules="[v => !!v || 'Title is required']"
+                v-model="title"
+              />
+              <v-textarea
+                label="Ad description"
+                name="description"
+                type="text"
+                :rules="[v => !!v || 'Description is required']"
+                v-model="description"
+              />
+            </v-form>
+            <v-layout row mb-3>
+              <v-flex xs12>
+                <v-btn class="warning">Upload
+                  <v-icon right dark>mdi-cloud-upload</v-icon>
+                </v-btn>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <img src="" alt="" height="100">
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <v-switch
+                  v-model="promo"
+                  label="Add to promo?"
+                  color="primary"
+                ></v-switch>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs12>
+                <v-spacer></v-spacer>
+                <v-btn
+                  class="success"
+                  @click="createAd"
+                  :disabled="!valid"
+                >Create ad</v-btn>
+              </v-flex>
+            </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +59,25 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      title: '',
+      description: '',
+      promo: false,
+      valid: false
+    }
+  },
+  methods: {
+    createAd () {
+      if (this.$refs.form.validate()) {
+        // logic
+        const ad = {
+          title: this.title,
+          description: this.description,
+          promo: this.promo
+        }
+        console.log(ad)
+      }
+    }
   }
 }
 </script>

595cd9cf0bd63eb19d9070a09dd66a6bb5b78205
Volkov Slava <volkov.slava@hotmail.com>
2020-03-10 23:17:11 +0300

    creating a list page

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,17 +1,58 @@
 <template>
   <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>AdList</h1>
-      </v-flex>
-    </v-layout>
+    <v-container grid-list-lg>
+      <v-layout row wrap justify-center>
+        <v-flex
+          xs12
+          sm6
+          md3
+          v-for="ad of ads"
+          :key="ad.id"
+        >
+          <v-card
+            class="mx-auto"
+            max-width="400"
+          >
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              :src="ad.imageSrc"
+            >
+            </v-img>
+              <v-card-title>{{ ad.title }}</v-card-title>
+            <v-card-text class="text--primary">
+              <div>{{ ad.description }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                class="primary"
+              >
+                Open
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-container>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      ads: [
+        {
+          title: 'First ad',
+          description: 'Hello i am description',
+          promo: false,
+          imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          id: '123'
+        }
+      ]
+    }
   }
 }
 </script>

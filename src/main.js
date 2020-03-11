da6d7d1dc59e3a5d47c04ebe1e77476c9af640c1
Volkov Slava <volkov.slava@hotmail.com>
2020-03-11 19:32:37 +0300

    configuring firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as fb from 'firebase'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
@@ -10,5 +11,16 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyD-gH3J2vRpmdaszhPLSrsXLde7_uf9t6c',
+      authDomain: 'itc-ads-7568c.firebaseapp.com',
+      databaseURL: 'https://itc-ads-7568c.firebaseio.com',
+      projectId: 'itc-ads-7568c',
+      storageBucket: 'itc-ads-7568c.appspot.com',
+      messagingSenderId: '465476665125',
+      appId: '1:465476665125:web:356aa0dfe4a1cc140d24cc'
+    })
+  }
 }).$mount('#app')

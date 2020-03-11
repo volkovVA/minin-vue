c7dee1c3244b7b520db01bba114b822ee16c5fd6
Volkov Slava <volkov.slava@hotmail.com>
2020-03-11 20:50:20 +0300

    registration of users

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -55,7 +55,8 @@
             <v-btn
               color="primary"
               @click="onSubmit"
-              :disabled="!valid"
+              :loading="loading"
+              :disabled="!valid || loading"
             >Create account</v-btn>
           </v-card-actions>
         </v-card>
@@ -86,6 +87,11 @@ export default {
       ]
     }
   },
+  computed: {
+    loading () {
+      return this.$store.getters.loading
+    }
+  },
   methods: {
     onSubmit () {
       if (this.$refs.form.validate()) {
@@ -93,7 +99,11 @@ export default {
           email: this.email,
           password: this.password
         }
-        console.log(user)
+        this.$store.dispatch('registerUser', user)
+          .then(() => {
+            this.$router.push('/')
+          })
+          .catch(err => console.log(err))
       }
     }
   }

e9041b9fa872c5be529362994f87b071994aa6b4
Volkov Slava <volkov.slava@hotmail.com>
2020-05-21 15:08:03 +0300

    added adding and editing categories

---
+++ src/store/category.js
@@ -0,0 +1,45 @@
+import firebase from 'firebase/app'
+
+export default {
+  actions: {
+    async fetchCategories ({ commit, dispatch }) {
+      try {
+        const uid = await dispatch('getUid')
+        const categories = (await firebase.database().ref(`/users/${uid}/categories`).once('value')).val() || {}
+        // const cats = []
+        // Object.keys(categories).forEach(key => {
+        //   cats.push({
+        //     title: categories[key].title,
+        //     limit: categories[key].limit,
+        //     id: key
+        //   })
+        // })
+        // return cats
+
+        return Object.keys(categories).map(key => ({ ...categories[key], id: key }))
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
+    },
+    async updateCategory ({ commit, dispatch }, { title, limit, id }) {
+      try {
+        const uid = await dispatch('getUid')
+        await firebase.database().ref(`/users/${uid}/categories`).child(id).update({ title, limit })
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
+    },
+    async createCategory ({ commit, dispatch }, { title, limit }) {
+      try {
+        const uid = await dispatch('getUid')
+        const category = await firebase.database().ref(`/users/${uid}/categories`).push({ title, limit })
+        return { title, limit, id: category.key }
+      } catch (error) {
+        commit('setError', error)
+        throw error
+      }
+    }
+  }
+}
